837ef8348da6bbadbe661bdeb2cb869a074d6730
Sebastiano Guerzoni <seba@inbitcoin.it>
2021-04-24 08:27:13 +0200

    Remove eth link

--- pywallet/utils/__init__.py
+++ pywallet/utils/__init__.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 
 from .bip32 import Wallet
-from .ethereum import (
-    HDPrivateKey, HDPublicKey, HDKey,
-    PrivateKey, PublicKey, Signature
-)
+# from .ethereum import (
+#    HDPrivateKey, HDPublicKey, HDKey,
+#    PrivateKey, PublicKey, Signature
+# )
 
 __all__ = [
     'Wallet',
 
-    'HDPrivateKey',
-    'HDPublicKey',
-    'HDKey',
-    'PrivateKey',
-    'PublicKey',
-    'Signature',
+    # 'HDPrivateKey',
+    # 'HDPublicKey',
+    # 'HDKey',
+    # 'PrivateKey',
+    # 'PublicKey',
+    # 'Signature',
 ]
